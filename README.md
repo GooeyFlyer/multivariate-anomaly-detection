a559387bab92cd97eaa390817ac4f25224b17f9e
GooeyFlyer <garrett.thomas.business@gmail.com>
2025-10-15 16:39:02 +0200

    bbn

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 ## Methods using multivariate data:
 - GRU (Gated Recurrent Units) - timeseries data, temperature_data.csv available from link at top of python code
 - Mahalanobis Distance - uses data.get_data()
-- dbscan - example uses 2D data for better visualisation
+- dbscan - example uses 2D data for better visualisation, uses data.get_data()
 - isolation forest - creditcard.csv available from link at top of python code
 - hierarchical_clustering - uses data.get_data()
+- BBN (Bayes Belief Network) - uses data from bnlearn package
 
 ## Methods using only 1D data:
 - POT (Peak Over Threshold)
--- data/get_data.py
+++ data/get_data.py
@@ -1,16 +1,18 @@
 import random
 import pandas as pd
 
-def get_data(num_datapoints: int, num_features: int) -> pd. DataFrame:
+
+def get_data(num_datapoints: int, num_features: int) -> pd.DataFrame:
     """Creates pandas Dataframe with random values 1-20, then appends anomaly (with value 40) as last data point"""
+
     # remember variable names are the features of a datapoint. Each index in the lists is a datapoint
     # random data then append datapoint with anomaly values
-
     data = {}
-    for num in range(1, num_features+1) :
-        data[f"Feature{str(num)}"] = [random. randint(1, 20) for _ in range(num_datapoints)] + [40]
+    for num in range(1, num_features+1):
+        data[f"Feature{str(num)}"] = [random.randint(1, 20) for _ in range(num_datapoints)] + [40]
 
     return pd.DataFrame(data)
+
 
 if __name__ == "__main__":
     print(get_data(10, 3))
